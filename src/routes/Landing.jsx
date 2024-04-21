cd58ed48014ed7403fa568467d9508bc7e7c235f
Zubaer Milky <62647046+zubaer012@users.noreply.github.com>
2024-04-21 14:40:12 -0400

    Updated logo and Main Page

--- src/routes/Landing.jsx
+++ src/routes/Landing.jsx
@@ -12,7 +12,15 @@ const Landing = ({ token, setToken }) => {
     <div className="landing-container">
       <div className="landing-content">
         <h1 className="landing-title">Your Crowdfunding Platform</h1>
-        {/* Add your styled text here */}
+        <h2 className="landing-h2">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </h2>
         <Link to="/" className="logout-btn" onClick={handleLogout}>
           Logout
         </Link>
