926157bc1ea267b393c7fa963f8e87dbfe75b934
Zubaer Milky <62647046+zubaer012@users.noreply.github.com>
2024-04-21 14:48:20 -0400

    Updated w text animations

--- src/routes/Landing.jsx
+++ src/routes/Landing.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import { Link } from "react-router-dom";
 import "./Landing.css";
 
@@ -8,18 +8,39 @@ const Landing = ({ token, setToken }) => {
     setToken(null);
   };
 
+  const typeRef = useRef(null);
+
+  useEffect(() => {
+    const text = typeRef.current.textContent.trim();
+    typeRef.current.textContent = ""; // Clear existing content
+
+    let i = 0;
+    const speed = 20;
+
+    const typeWriter = () => {
+      if (i < text.length) {
+        typeRef.current.textContent += text.charAt(i);
+        i++;
+        setTimeout(typeWriter, speed);
+      }
+    };
+
+    typeWriter();
+  }, []);
+
   return (
     <div className="landing-container">
       <div className="landing-content">
-        <h1 className="landing-title">Your Crowdfunding Platform</h1>
-        <h2 className="landing-h2">
+        <h1 className="landing-name">JumpStart.</h1>
+        <h1 className="landing-title">Your Journey Starts here.</h1>
+        <h2 ref={typeRef} className="landing-h2">
+          {/* Add your text content here */}
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
           eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
           minim veniam, quis nostrud exercitation ullamco laboris nisi ut
           aliquip ex ea commodo consequat. Duis aute irure dolor in
           reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
+          pariatur.
         </h2>
         <Link to="/" className="logout-btn" onClick={handleLogout}>
           Logout
