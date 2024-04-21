b909e608b4b6b72aeb14723a1c39023d186165f2
Arjun Agarwal <arjunagw02@gmail.com>
2024-04-21 14:55:26 -0400

    began creation of about page

--- src/routes/About.jsx
+++ src/routes/About.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import {Carousel, Card, Button, Image} from 'react-bootstrap'
+import {Container, Row, Col} from 'react-bootstrap'
+import aboutHeader from '../assets/images/about-page-header-img.jpeg'
+import './About.css'
 
 const About = () => {
   return (
-    <div>About</div>
+    <Container>
+      <Row className='mt-5'>
+        <Col>
+        <h1>About</h1>
+        <p>At JumpStart, </p>
+        </Col>
+        <Col className='d-flex justify-content-center'>
+          <div className='image-container'>
+            <Image src={aboutHeader} fluid />
+          </div>
+        </Col>
+      </Row>
+    </Container>
   )
 }
 
