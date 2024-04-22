13ca64a2d4636d24b8a19152be95c8abb241f00d
Harshavardan Yuvaraj <harshavardanyuvaraj@gmail.com>
2024-04-22 16:18:42 -0400

    Project Page Created & 50% Done

--- src/routes/ProjectPage.jsx
+++ src/routes/ProjectPage.jsx
@@ -1,11 +1,47 @@
-import React from 'react'
-import { useParams } from 'react-router-dom'
+import React, { useState, useEffect } from 'react'
+import { useParams, useNavigate } from 'react-router-dom'
+import {Stack, Image, Row, Col, Container} from 'react-bootstrap'
+import './ProjectPage.css'
+import Compass from '../assets/compass.png'
+import database from '../client';
+
 
 const ProjectPage = () => {
+  const navigate = useNavigate();
   const {pid} = useParams();
+  const [project, setProject] = useState(false);
+  
+  useEffect(()=>{
+     const getProject = async () => {
+        const {data, error} = await database.from('projects').select().eq('pid',pid);
+        
+        if(data)
+          setProject(data[0]);
+     }
+     getProject();
+
+  },[]);
 
   return (
-    <div>Project Page: {pid}</div>
+    <>
+      {project && 
+          <Stack gap={4}>
+              <h1 className='abril-fatface-regular mt-3 mx-auto'>{project.title}</h1>
+              <hr />
+             <Stack direction='horizontal' gap={3}>
+                <Image className='mx-auto p-2' src={project.img_url} width='25%' height='25%' fluid rounded/>
+                <Stack direction='vertical' className='mx-auto'>
+                     <h3><Image src={Compass} width='25px'  height='25px'/> <span className='teko-category p-2'>{ project.category}</span></h3>
+                     <h2 className='mt-2 money'>{`$${(project.amt_pledged).toLocaleString()} Pledged`} <h4 style={{display: 'inline', color:'black'}}>of {`$${project.amt_requested} goal.`}</h4></h2>
+                     <p className='mt-2'>{project.text}</p>
+                </Stack>
+             </Stack>
+
+             
+          </Stack>
+      }
+
+    </>
   )
 }
 
