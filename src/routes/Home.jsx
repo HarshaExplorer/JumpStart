1d5f85405fcd27b5215631f2ed0f0491f79cc52d
Harsha Yuvaraj <harshavardanyuvaraj@gmail.com>
2024-04-24 23:11:44 -0400

    Home Page Merged

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,14 +1,36 @@
-import React, { useEffect } from 'react'
+import React from "react";
+import "./Home.css";
+import { Typewriter } from "react-simple-typewriter";
 
-const Home = () => {
-
-  useEffect(()=>{
-    document.body.style.backgroundColor = '#272b33';
-  }, []);
-
+const Home = ({ token, setToken }) => {
   return (
-    <div>Home</div>
-  )
-}
+    <div className="landing-container">
+      <div className="landing-content">
+        <h1 className="landing-name kanit-bold">
+          <span style={{ color: "#6ee397" }}>Jump</span>Start
+        </h1>
+        <h1 className="landing-title sanchez-regular">
+          Empowering Innovators, Fueling Dreams.
+        </h1>
 
-export default Home
+        <h2>
+          <span style={{ color: "white" }}>We're</span>{" "}
+          <span className="typewriter-style">
+            <Typewriter
+              words={[
+                "more than just a platform.",
+                "a launchpad for your dreams.",
+                "a bridge that brings your projects to life.",
+              ]}
+              loop={true}
+              cursor
+              typeSpeed={40}
+            />
+          </span>
+        </h2>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
